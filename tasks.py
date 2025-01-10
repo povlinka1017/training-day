ba8104117786a1090ed6bad6f634b5f3a06723a5
ext-deviget-oleh.pavlenko <oleh.pavlenko@squad.tech>
2025-01-10 18:59:18 +0200

    [Task] https://www.codewars.com/kata/52597aa56021e91c93000cb0/python
    [Solution]
    concatenation of 2 list variables with non-zero and zero items

---
+++ tasks.py
@@ -0,0 +1,13 @@
+def move_zeros(lst: list):
+    """
+    Write an algorithm that:
+    - takes an array
+    - moves all of the zeros to the end, preserving the order of the other elements.
+    :param lst:
+    :return: list
+    """
+    non_zero: list = [i for i in lst if i != 0]
+    nulls: list = [i for i in lst if i == 0]
+    return non_zero + nulls
+
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
