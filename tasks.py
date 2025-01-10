bc388bd9da655219fdd59d0b332bc8acba433983
ext-deviget-oleh.pavlenko <oleh.pavlenko@squad.tech>
2025-01-10 19:02:44 +0200

    [Task] https://www.codewars.com/kata/52597aa56021e91c93000cb0/python
    [Solution]
    concatenation of 2 list variables with non-zero and zero items

--- tasks.py
+++ tasks.py
@@ -10,4 +10,6 @@ def move_zeros(lst: list):
     nulls: list = [i for i in lst if i == 0]
     return non_zero + nulls
 
-print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+print(
+    move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9])
+)
