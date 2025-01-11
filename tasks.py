19419176c104f7350bd2f4c65332402c8f5a7b4c
Oleh Pavlenko <povlinka1017@gmail.com>
2025-01-11 14:06:33 +0200

    log

--- tasks.py
+++ tasks.py
@@ -10,6 +10,18 @@ def move_zeros(lst: list):
     nulls: list = [i for i in lst if i == 0]
     return non_zero + nulls
 
-print(
-    move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9])
-)
+#print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+
+
+def expanded_form(num: int):
+    num_list = [int(i) for i in str(num)]
+    lst = []
+
+    for index, digit in enumerate(num_list):
+        print(index, digit)
+        if digit != 0:
+            place_value = digit * 10 ** (len(num_list) - index - 1)
+            lst.append(str(place_value))
+    return ' + '.join(map(str, lst))
+
+print(expanded_form(10020062))
